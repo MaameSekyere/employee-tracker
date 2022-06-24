44cf254a8512db86ee9e73427fbc996b2e50e085
MaameSekyere <maameant@gmail.com>
2022-06-24 10:22:03 -0400

    revised db/

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,29 @@
-DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS roles;
-DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS employee;
 
 
 CREATE TABLE departments (id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-department_name VARCHAR
-  (30) NOT NULL
+department_name VARCHAR (30) NOT NULL
 );
 
 CREATE TABLE roles (id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-role_name VARCHAR (30),
-salary INT,
+title VARCHAR (30) NOT NULL,
+salary DECIMAL NOT NULL,
 department_id INT,
-FOREIGN KEY (department_id) REFERENCES departments (id) ON DELETE CASCADE
+FOREIGN KEY (department_id) REFERENCES departments (id)
 );
 
-CREATE TABLE employees (id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR (30), last_name VARCHAR (30),
-role_id INT, manager_id INT, FOREIGN KEY (manager_id) REFERENCES employees (id),
-FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE );
+CREATE TABLE employee (id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+first_name VARCHAR (30) NOT NULL, 
+last_name VARCHAR (30) NOT NULL,
+role_id INT, 
+manager_id INT, 
+FOREIGN KEY (manager_id) REFERENCES roles (id) ON DELETE SET NULL,
+FOREIGN KEY (role_id) REFERENCES roles(id) 
+);
 
- -- displays formatted table with only relevant info
- SELECT e.first_name, e.last_name, r.role_name, r.salary, e.manager_id, m.first_name AS manager_firstname, m.last_name AS manager_lastname, r.department_id FROM employee_db.employees e INNER JOIN employee_db.employees m ON e.manager_id = m.id INNER JOIN employee_db.roles r ON e.role_id = r.id INNER JOIN employee_db.departments d ON r.department_id = d.id
+ 
 
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO departments
+INSERT INTO department
     (department_name)
   VALUES
     ("Engineering"),
@@ -6,16 +6,19 @@ INSERT INTO departments
     ("Maintenance"),
     ("Accounting");
 
-INSERT INTO roles (role_name, salary, department_id)
-VALUES ("Senior Engineer", 50000, 1),
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Senior Engineer", 150000, 1),
       ("Intern", 30000, 1),
       ("Salesperson", 35000, 2),
       ("Attourney", 50000, 3),
       ("Accountant", 60000, 4);
 
-INSERT INTO employees (first_name, last_name, role_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
         ("Maame", "Sekyere", 1),
         (),
         ("Sylvia", "Ayisi", 4),
-        ("Jeff", "Owusu", 3);
+        ("Jeff", "Owusu", 3),
+        ('Lina', 'Wilson',2,NULL),
+        ('David', 'Green',4,4),
+        ('Taylor', 'Moore',3,NUll);
